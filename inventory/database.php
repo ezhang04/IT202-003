a3849602b346be73794a5c25230cafe5ea1cee0d
ezhang <97803134+ezhang04@users.noreply.github.com>
2024-09-10 17:35:58 -0400

    Added More Dummy Values

---
+++ inventory/database.php
@@ -0,0 +1,19 @@
+<?php
+function getDB() {
+   $host = 'sql1.njit.edu';
+   $port = 3306;
+   $dbname = 'ez57';
+   $username = 'ez57';
+   $password = '242426Protagonist';
+   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+   try {
+       $db = new mysqli($host, $username, $password, $dbname, $port);
+ echo "You are connected to the $host database!";
+       return $db;
+   } catch (mysqli_sql_exception $e) {
+       error_log($e->getMessage(), 0);
+       echo $e->getMessage();
+   }
+}
+getDB();
+?>
