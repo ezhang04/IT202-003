98af08989194a55cfa7c75e74bb6a01c3cab1410
ezhang <97803134+ezhang04@users.noreply.github.com>
2024-09-19 10:22:31 -0400

    Created dummy values in database and allow user to login and log out

--- inventory/database.php
+++ inventory/database.php
@@ -4,16 +4,16 @@ function getDB() {
    $port = 3306;
    $dbname = 'ez57';
    $username = 'ez57';
-   $password = '242426Protagonist';
+   $password = '242426_Protagonist';
    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
    try {
        $db = new mysqli($host, $username, $password, $dbname, $port);
- echo "You are connected to the $host database!";
+       error_log("You are connected to the $host database!");
        return $db;
    } catch (mysqli_sql_exception $e) {
        error_log($e->getMessage(), 0);
        echo $e->getMessage();
    }
 }
-getDB();
+//getDB();
 ?>
